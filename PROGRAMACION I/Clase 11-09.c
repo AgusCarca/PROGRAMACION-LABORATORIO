eba23be673121d5b464b2395ba65c53c6fda2c33
Agustin <aguscarcacha@gmail.com>
2020-09-11 11:46:46 -0300

    clase 11-09

--- PROGRAMACION I/Clase 11-09.c
+++ PROGRAMACION I/Clase 11-09.c
@@ -1,5 +1,31 @@
-void main ()
-{
-    printf ("Hola iriotas");
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main ()
+{
+    char palabra [15];
+    int len;
+    printf ("Ingrese un mensaje: ");
+    //scanf ("%s", palabra);
+    fgets(palabra, 15, stdin);
+    len = strlen(palabra);
+    /*char otraPalabra [10]="Gato";
+    int comp;
+    comp = strcmp(palabra, otraPalabra); //DEVUELVE 0 SI LAS CADENAS SON IGUALES
+
+    printf ("%d\n", comp)
+
+    //strcpy (otraPalabra, palabra);
+    //strcpy (palabra, "Gato");
+
+    /*int len;
+    len = strlen ("El sol");
+    */
+
+    puts(palabra);
+    printf ("%d", len);
+    //printf ("len: %d", len);
+
+    return 0;
 }
